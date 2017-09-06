26367f04117c68c1f4617f4440a006231fb9b1f4
Marcos Mota <marcosfmmota@gmail.com>
2017-09-06 12:05:33 -0300

    negative implementation

--- main.py
+++ main.py
@@ -2,6 +2,28 @@
 
 import os
 from skimage import io
+import matplotlib.pyplot as plt
+import numpy as np
 
-int main():
-    dir_name = ""
+
+def negative(image):
+
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            image[i, j] = 255 - image[i, j]
+
+
+def main():
+
+    dir_name = "/home/marcosfe/Documents/PhotoTops/DIP3E_CH03"
+    filename = input("Type the image name \n")
+    filename = os.path.join(dir_name,filename)
+    image = io.imread(filename)
+    print(image.shape)
+    negative(image)
+    plt.imshow(image, cmap = "gray")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
