230985e6745b1a873f634d022c6967ca85699f11
Marcos Mota <marcosfmmota@gmail.com>
2017-09-06 12:49:24 -0300

    power_transform

--- main.py
+++ main.py
@@ -12,12 +12,17 @@ def negative(image):
         for j in range(image.shape[1]):
             image[i, j] = 255 - image[i, j]
 
-def log_transform(image, c = 1):
+def log_transform(image, c = 1.0):
 
     for i in range(image.shape[0]):
         for j in range(image.shape[1]):
             image[i, j] = c * math.log(1 + image[i, j], 2)
 
+def power_transform(image, c = 1.0, y = 1):
+
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            image[i, j] = c * math.pow(image[i, j], y)
 
 def main():
 
@@ -26,6 +31,11 @@ def main():
     filename = os.path.join(dir_name,filename)
     image = io.imread(filename)
     print(image.shape)
+    plt.imshow(image, cmap="gray")
+    plt.show()
+    power_transform(image, 1, 0.4)
+    plt.imshow(image, cmap="gray")
+    plt.show()
     log_transform(image)
     plt.imshow(image, cmap="gray")
     plt.show()
