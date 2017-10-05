6472292b3b4133eddca6daf460659569e27a2cbc
Marcos Mota <marcosfmmota@gmail.com>
2017-10-05 01:01:40 -0300

    Fully working local histogram equalization (finally!)

--- tests_CHXX.py
+++ tests_CHXX.py
@@ -119,9 +119,13 @@ def test_local_histogram_equalization(dirname, filename):
 
     filename = os.path.join(dirname, filename)
     image = io.imread(filename)
+    fig = plt.figure("Local Histogram Equalization")
+    ax = fig.add_subplot(1, 2, 1)
+    ax.set_title("Original Image")
     plt.imshow(image, cmap="gray")
-    plt.show()
-    image = sf.local_equalization(image)
+    ax = fig.add_subplot(1, 2, 2)
+    ax.set_title("Equalized Image")
+    sf.local_equalization(image)
     plt.imshow(image, cmap="gray")
     plt.show()
 
@@ -157,11 +161,9 @@ def test_median(dirname, filename):
     ax = fig.add_subplot(1, 2, 2)
     ax.set_title("Blured")
     kernel = np.array([
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1]
+        [1, 1, 1],
+        [1, 1, 1],
+        [1, 1, 1],
     ])
     plane = sf.convolve_median(image, kernel)
     plt.imshow(plane, cmap="gray")
@@ -252,16 +254,15 @@ def test_batch_CH03():
     # test_bit_plane_slicing(dir_name, "Fig0314(a)(100-dollars).tif")
     # test_contrast_stretching(dir_name, "Fig0312(a)(kidney).tif")
     # test_histogram(dir_name, "Fig0316(1)(top_left).tif")
-    # test_histogram(dir_name, "Fig0316(2)(2nd_from_top).tif")
     # test_histogram_equalization(dir_name, "Fig0309(a)(washed_out_aerial_image).tif")
-    # test_local_histogram_equalization(dir_name, "Fig0326(a)(embedded_square_noisy_512).tif")
+    test_local_histogram_equalization(dir_name, "Fig0326(a)(embedded_square_noisy_512).tif")
     # test_average(dir_name, "Fig0333(a)(test_pattern_blurring_orig).tif")
     # test_median(dir_name, "Fig0335(a)(ckt_board_saltpep_prob_pt05).tif")
     # test_highboost(dir_name, "Fig0340(a)(dipxe_text).tif")
     # test_laplace(dir_name, "Fig0338(a)(blurry_moon).tif")
     # test_sobel_x(dir_name, "Fig0338(a)(blurry_moon).tif")
     # test_sobel_y(dir_name, "Fig0338(a)(blurry_moon).tif")
-    test_enhanced_borders_sobel(dir_name, "Fig0338(a)(blurry_moon).tif")
+    # test_enhanced_borders_sobel(dir_name, "Fig0338(a)(blurry_moon).tif")
 
 
 def test_shrink_image(dirname, filename):
@@ -274,7 +275,7 @@ def test_shrink_image(dirname, filename):
     plt.imshow(image, cmap="gray")
     ax = fig.add_subplot(1, 2, 2)
     ax.set_title("Shrinked Image")
-    shrink = ff.shrink_image(image, 3)
+    shrink = ff.shrink_image(image, 2)
     plt.imshow(shrink, cmap="gray")
     plt.show()
 
@@ -289,8 +290,22 @@ def test_shrink_average_image(dirname, filename):
     plt.imshow(image, cmap="gray")
     ax = fig.add_subplot(1, 2, 2)
     ax.set_title("Shrinked Image")
-    shrink = ff.shrink_average_image(image, 3)
+    shrink = ff.shrink_average_image(image, 2)
     plt.imshow(shrink, cmap="gray")
+    plt.show()
+
+def test_compute_spectrum(dirname, filename):
+
+    filename = os.path.join(dirname, filename)
+    image = io.imread(filename)
+    fig = plt.figure("Border detection")
+    ax = fig.add_subplot(1, 2, 1)
+    ax.set_title("Original Image")
+    plt.imshow(image, cmap="gray")
+    ax = fig.add_subplot(1, 2, 2)
+    ax.set_title("Shrinked Image")
+    spectrum = ff.compute_spectrum(image)
+    plt.imshow(spectrum, cmap="gray")
     plt.show()
 
 
@@ -299,3 +314,6 @@ def test_batch_CH04():
     dir_name = "C:\\Users\\MarcosFelipe\\Documents\\PhotoTops\\DIP3E_CH04"
     test_shrink_image(dir_name, "Fig0417(a)(barbara).tif")
     test_shrink_average_image(dir_name, "Fig0417(a)(barbara).tif")
+    test_compute_spectrum(dir_name, "Fig0424(a)(rectangle).tif")
+    test_compute_spectrum(dir_name, "Fig0427(a)(woman).tif")
+
